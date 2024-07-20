980c94196edd4f5b7f3c5fd11c35dda0863fd410
c-rew1nd <dreektseayger@gmail.com>
2024-07-20 22:28:19 +0530

    comited 02_datatypes.js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert(3+3) // will give error when run using node.js not browser
+
+let name = "Dreekt" //string 
+let age = 29 //number 
+let isLoggedIn = true // boolean 
+
+
+// number 
+// bigInt 
+// string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined => 
+// symbol => used for uniqueness
+
+// object 
+
+console.log(typeof "Dreekt");
+console.log(typeof age);
+console.log(typeof null); // type => object
+console.log(typeof undefined); // type => undefined
