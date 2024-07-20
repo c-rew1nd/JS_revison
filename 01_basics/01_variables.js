35b666e2ca02b9631afcd9ca6444e0abf8b780e6
c-rew1nd <dreektseayger@gmail.com>
2024-07-20 21:53:38 +0530

    Added Variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accoountEmail = "123hello@gmail.com" 
+var accountPass = "12345"
+accountCity = "Delhi"
+let accountState;
+/*
+Don't use var as it can cause "Block and Funtional Scope Problem".
+*/ 
+
+console.table([accountId, accoountEmail,accountPass, accountCity,accountState])
+  
